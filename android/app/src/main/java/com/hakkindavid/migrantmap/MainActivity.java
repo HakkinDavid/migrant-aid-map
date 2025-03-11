53086614efb1d253d7657cee388da7d5c86ddee4
HakkinDavid <desr11@hotmail.com>
2025-03-10 22:33:17 -0700

    app and location

---
+++ android/app/src/main/java/com/hakkindavid/migrantmap/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hakkindavid.migrantmap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,18 +1,25 @@
 <script>
     import { onMount, onDestroy, afterUpdate } from 'svelte';
 
+    const TIJUANA_CENTER = [32.522499, -117.046623];
+    
     export let markers = [];
-
+    export let m_id = [];
+    
+    let positionMarker;
     let mapElement;
     let map;
+    let dotIcon;
+    let posWatcher;
 
     async function buildMap () {
         const leaflet = await import('leaflet');
+        const { Geolocation } = await import('@capacitor/geolocation');
 
         await removeMap();
 
         map = leaflet.map(mapElement, {
-            center: [32.52, -117.02],
+            center: TIJUANA_CENTER,
             zoom: 10.33,
             maxBounds: [
                 [32.532838, -117.134573],
@@ -21,7 +28,7 @@
                 [32.280616, -116.524034]
             ],
             maxBoundsViscosity: 1.0
-        }).setView([32.522499, -117.046623], 13);
+        }).setView(TIJUANA_CENTER);
     
         leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -29,21 +36,68 @@
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
 
-        markers.forEach(m => {
-            m.element = leaflet.marker(m.geo).addTo(map);
-            m.element.bindPopup(m.name);
-        });
+        await addMarkers();
+
+        if (!dotIcon) {
+            dotIcon = leaflet.icon({iconUrl: '/dot.png', iconSize: [50,50]});
+        }
+        
+        posWatcher = await Geolocation.watchPosition({}, updatePositionMarker);
     }
 
     async function removeMap () {
         if (map) {
             map.remove();
         }
+        if (posWatcher) {
+            const { Geolocation } = await import('@capacitor/geolocation');
+            Geolocation.clearWatch({id: posWatcher});
+        }
+        positionMarker = undefined;
+        dotIcon = undefined;
+        posWatcher = undefined;
+    }
+
+    async function addMarkers () {
+        if (map) {
+            const leaflet = await import('leaflet');
+            markers.forEach(m => {
+                m_id.push(leaflet.marker(m.geo).addTo(map).bindPopup(m.name));
+            });
+        }
+    }
+
+    function removeMarkers () {
+        if (map) {
+            m_id.forEach(m => {
+                map.removeLayer(m);
+            });
+        }
+    }
+
+    async function updatePositionMarker (position) {
+        if (!map || !position) return;
+
+        if (positionMarker) {
+            console.log('Updated position to ' + position.coords.latitude + ',' + position.coords.longitude);
+            positionMarker.setLatLng([position.coords.latitude, position.coords.longitude]);            
+        }
+        else {
+            const leaflet = await import('leaflet');
+            positionMarker = leaflet.marker([position.coords.latitude, position.coords.longitude], {icon: dotIcon}).addTo(map);
+            positionMarker.bindPopup("Ubicación actual");
+            map.setView([position.coords.latitude, position.coords.longitude], 13);
+        }
+    }
+
+    async function updateMap () {
+        removeMarkers();
+        await addMarkers();
     }
 
     onMount(buildMap);
 
-    afterUpdate(buildMap);
+    afterUpdate(updateMap);
 
     onDestroy(removeMap);
 </script>
